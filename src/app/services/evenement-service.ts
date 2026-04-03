bba1e54e9fff156f26da09e08e15e5162eae6616
achrafLIHEOUEL <liheouelachraf2001@gmail.com>
2026-04-03 15:37:53 +0100

    backEvent , partenariat

--- src/app/services/evenement-service.ts
+++ src/app/services/evenement-service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class EvenementService {
- private apiUrl = 'http://localhost:8081/api/evenements';
+ private apiUrl = 'http://localhost:8080/api/evenements';
 
   constructor(private http: HttpClient) {}
 
